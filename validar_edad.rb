20ab61ad051f4871e4766e138093c9f7c0814526
alrivasp <alvaro.rivas.p@gmail.com>
2020-05-09 21:49:46 -0400

    Up Desafio validad edad

---
+++ validar_edad.rb
@@ -0,0 +1,13 @@
+def validar_edad  edad  
+    if edad >= 18
+        puts "es mayor"
+    else
+        puts "es menor"
+    end
+end
+
+validar_edad edad= rand(1..99)
+
+validar_edad edad= rand(1..99)
+
+validar_edad edad= rand(1..99)
